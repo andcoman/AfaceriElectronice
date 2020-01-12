7cc9f15c285045b54cfde7933a74d2a3eb888c35
andcoman <59795152+andcoman@users.noreply.github.com>
2020-01-12 15:36:16 +0200

    Add test perf and doc

--- PiesaDeSchimb.ps1
+++ PiesaDeSchimb.ps1
@@ -5,7 +5,27 @@ Write-Output -InputObject $stepName
 
 for($i = 0; $i -lt 5; ++$i)
 {
-    $executioTime += Measure-Command { Invoke-RestMethod -Method Get -Uri https://localhost:44310/ -UseBasicParsing }
+    $executioTime += Measure-Command { Invoke-RestMethod -Method Get -Uri https://piesadeschimb20200112022454.azurewebsites.net/ -UseBasicParsing }
 }
 
-[System.Console]::WriteLine($stepName + " a durat " + $($executioTime.Seconds / 5) + " secunde - medie 5 iteratii"); Remove-Variable executioTime
+[System.Console]::WriteLine($stepName + " a durat " + $($executioTime.TotalMilliseconds / 5) + " milisecunde - medie 5 iteratii"); Remove-Variable executioTime
+
+$stepName = "2. Vizualizare produs"
+Write-Output -InputObject $stepName 
+
+for($i = 0; $i -lt 5; ++$i)
+{
+    $executioTime += Measure-Command { Invoke-RestMethod -Method Get -Uri https://piesadeschimb20200112022454.azurewebsites.net/Product/Details/5e10cd3ca5f6d05a68d533e1 -UseBasicParsing }
+}
+
+[System.Console]::WriteLine($stepName + " a durat " + $($executioTime.TotalMilliseconds / 5) + " millisecunde - medie 5 iteratii"); Remove-Variable executioTime
+
+$stepName = "3. Creare produs"
+Write-Output -InputObject $stepName 
+
+for($i = 0; $i -lt 5; ++$i)
+{
+    $executioTime += Measure-Command { Invoke-RestMethod -Method Post -Uri https://piesadeschimb20200112022454.azurewebsites.net/Product/Create -UseBasicParsing }
+}
+
+[System.Console]::WriteLine($stepName + " a durat " + $($executioTime.TotalMilliseconds / 5) + " milisecunde - medie 5 iteratii"); Remove-Variable executioTime
